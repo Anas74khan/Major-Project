3a608f4f994089786a9f50e4a00795ae6f837ca5
Mohd Anas Khan <mdarfankhan9@gmail.com>
2022-03-27 15:51:32 +0530

    Footer and UI changes
    Footer created and updated some UI.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,42 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='footer-container'>
+
+        <div className='links'>
+          <div className='link-heading'>About</div>
+          <div className='link'>Contact Us</div>
+          <div className='link'>About Us</div>
+          <div className='link'>Blogs</div>
+        </div>
+
+        <div className='links'>
+          <div className='link-heading'>Help</div>
+          <div className='link'>Cancellation &amp; Return</div>
+          <div className='link'>FAQ</div>
+        </div>
+
+        <div className='links'>
+          <div className='link-heading'>Policy</div>
+          <div className='link'>Return Policy</div>
+          <div className='link'>Privacy Policy</div>
+          <div className='link'>Terms &amp; Conditions</div>
+          <div className='link'>Security Policy</div>
+        </div>
+
+        <div className='links'>
+          <div className='link-heading'>Social</div>
+          <div className='link'>LinkedIn</div>
+          <div className='link'>Facebook</div>
+          <div className='link'>Instagram</div>
+          <div className='link'>Twitter</div>
+        </div>
+
+      </div>
+      <div className='footer-bottom'>
+        Developed by Major Project Team
+      </div>
+    </div>
   )
 }
